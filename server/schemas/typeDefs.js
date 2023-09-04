876e05ad6749e4965dc87b09b0274cb5271c3b28
Houston Culpepper <howacul@gmail.com>
2023-09-04 18:29:35 -0400

    added typeDefs to schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,4 +1,44 @@
-const typeDefs = require("./typeDefs");
-const resolvers = require("./resolvers");
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
 
-module.exports = { typeDefs, resolvers };
+  type Book {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type BookInput {
+    bookId: String!
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+module.exports = typeDefs;
