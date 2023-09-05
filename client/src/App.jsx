0c721dfd93e2283cb4f97c714f41e7215409db3a
Houston Culpepper <howacul@gmail.com>
2023-09-04 21:15:45 -0400

    updated App.jsx to include Apollo Provider to make request work with Apollo Server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,43 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  setContext,
+} from "@apollo/client";
 
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat("/graphql"),
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" cinponent={SearchBooks} />
+          <Route exact path="/saved" component={SavedBooks} />
+          <Route exact path="/search" component={SearchBooks} />
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
